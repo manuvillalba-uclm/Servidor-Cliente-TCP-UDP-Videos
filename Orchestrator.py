9f435d4dc80679f0cd2ec1572f6ea6c11d80ec7e
manuel <mvillalba@gmail.com>
2019-11-16 15:08:52 +0100

    V1.0

--- Orchestrator.py
+++ Orchestrator.py
@@ -9,31 +9,40 @@ Ice.loadSlice('trawlnet.ice')
 import TrawlNet
 
 
-class Orchestrator1(TrawlNet.Orchestrator):
+class Orchestrator1(TrawlNet.Orchestrator, Ice.Application):
     n = 0
 
     def downloadTask(self, message, current=None):
         print("{0}: {1}".format(self.n, message))
         sys.stdout.flush()
         self.n += 1
+        proxy = Server.communicator().stringToProxy("downloader -t -e 1.1:tcp -h 192.168.1.43 -p 9090 -t 60000")
+        msg = TrawlNet.DownloaderPrx.checkedCast(proxy)
+        msg.addDownloadTask(message)
+
 
 
 class Server(Ice.Application):
     def run(self, argv):
+
         broker = self.communicator()
         servant = Orchestrator1()
 
         adapter = broker.createObjectAdapter("OrchestratorAdapter")
         proxy = adapter.add(servant, broker.stringToIdentity("orchestrator"))
-
         print(proxy)
+
         sys.stdout.flush()
 
         adapter.activate()
         self.shutdownOnInterrupt()
         broker.waitForShutdown()
-
         return 0
+
+
+
+
+
 
 server = Server()
 sys.exit(server.main(sys.argv))
