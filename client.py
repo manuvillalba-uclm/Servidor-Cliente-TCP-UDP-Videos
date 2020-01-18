2a1e994494c3f1143146308003b59ec4c592fec0
manuel <manu.del.terry@gmail.com>
2020-01-18 14:59:51 +0100

    client.py 10/10 en pylint
    solucionado url incorrecta

--- client.py
+++ client.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+"""client.py
+Creado por Manuel Villalba y Luis Pajarero
+Sistemas Distribuidos 2019/2020
+
+Utilizado por el usuario para comunicarse con el servidor, el proxy con el que se
+comunica es el grupo de réplica orchestrator, las opciones:
+
+    no opción: Solicita la lista completa de descargas.
+
+    --download <url>: Solicitar una descarga de una url válida.
+
+    --transfer <nombre.mp3>: Solicita una transfererencia del archivo nombre.mp3
+"""
 
 import binascii
 import sys
-import Ice
 import getopt
 import os
+import Ice
+
+
 
 Ice.loadSlice('trawlnet.ice')
+# desctivamos porque import TrawlNet funciona correctamente pero sólo después de hacer
+# Ice.loadSlice('trawlnet.ice')
+
+# pylint: disable=E0401,C0413
 import TrawlNet
 
 APP_DIRECTORY = './'
@@ -15,12 +34,32 @@ DOWNLOADS_DIRECTORY = os.path.join(APP_DIRECTORY, 'downloads')
 
 
 class Client(Ice.Application):
+    """clase Client
+    Creado por Manuel Villalba y Luis Pajarero
+    Sistemas Distribuidos 2019/2020
 
-    FileList = []
+    Clase principal de cliente.
+
+    Contiene run y transfer_request, este último aportado por los profesores de la asignatura.
+    """
+    filelist = []
     orchestrator = None
 
     def transfer_request(self, file_name):
+        """clase Client
+        Creado por Manuel Villalba y Luis Pajarero
+        Sistemas Distribuidos 2019/2020
+
+        transfer_request(self, file_name):
+
+        Argumentos:
+            file_name: nombre del archivo recibido por argumentos cuando la opción --transfer
+                está activa.
+        """
+        # Nombres aportados por los profesores
+        # pylint: disable=C0103
         remote_EOF = False
+        # pylint: disable=C0103
         BLOCK_SIZE = 1024
         transfer = None
 
@@ -29,10 +68,11 @@ class Client(Ice.Application):
         except TrawlNet.TransferError as e:
             print(e.reason)
             return 1
-        except Exception as e:
-            print("El archivo no existe!, a continuación se mostrará la lista de archivos descargados actualmente:")
-            self.FileList = self.orchestrator.getFileList()
-            print(self.FileList)
+        except Ice.Exception as e:
+            print("El archivo no existe!, "
+                  "a continuación se mostrará la lista de archivos descargados actualmente:")
+            self.filelist = self.orchestrator.getFileList()
+            print(self.filelist)
             sys.stdout.flush()
             return 1
 
@@ -51,7 +91,8 @@ class Client(Ice.Application):
 
         transfer.destroy()
         print('Transfer finished!')
-
+    #Le tengo que pasar los argumentos de alguna forma..
+    # pylint: disable=W0221
     def run(self, argv):
         #Compruebo el proxy
         proxy = self.communicator().stringToProxy(argv[1])
@@ -63,29 +104,40 @@ class Client(Ice.Application):
 
         #Compruebo las opciones
         try:
+            #args sí se usa pero como arg
+            # pylint: disable=W0612
             opts, args = getopt.getopt(sys.argv[2:], "", ["download=", "transfer="])
         except getopt.GetoptError as err:
             print(err)
             sys.exit(2)
 
         if len(opts) == 0:
-            self.FileList = self.orchestrator.getFileList()
-            print(self.FileList)
+            self.filelist = self.orchestrator.getFileList()
+            print(self.filelist)
         for opt, arg in opts:
             if opt == "--download":
                 print("DOWNLOAD")
                 sys.stdout.flush()
                 url = arg
-                val = self.orchestrator.downloadTask(url)
-                if val.name == "REPETIDO" and val.hash == "":
-                    print("Este archivo ya está descargado en el Servidor")
-                    print("A continuación se mostrará la lista de archivos descargados actualmente:")
-                    self.FileList = self.orchestrator.getFileList()
-                    print(self.FileList)
-                    sys.stdout.flush()
-                else:
-                    print(val)
-                    sys.stdout.flush()
+                try:
+                    val = self.orchestrator.downloadTask(url)
+                    if val.name == "REPETIDO" and val.hash == "":
+                        print("Este archivo ya está descargado en el Servidor")
+                        print("A continuación se mostrará la lista de archivos "
+                              "descargados actualmente:")
+                        self.filelist = self.orchestrator.getFileList()
+                        print(self.filelist)
+                        sys.stdout.flush()
+                    else:
+                        print(val)
+                        sys.stdout.flush()
+
+                except Ice.Exception as error:
+                    print(error)
+                    print("URL incompleta o red no disponible.")
+                    return 0
+
+
 
             elif opt == "--transfer":
                 print("TRANSFER")
